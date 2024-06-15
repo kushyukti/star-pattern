b78cb68a521e02f8ea1239be3fd81ca01733f0b8
Yukti Kushwaha <yukti.kushwaha@tatasteel.com>
2024-06-16 02:24:25 +0530

    day 1 done

---
+++ pattern_1.py
@@ -0,0 +1,11 @@
+def pattern_1():
+
+  for i in range(5):
+    for j in range(5):
+        if(j<=i):
+          print("*", end=" ")
+        else:
+          print(" ", end=" ")
+    print("\n")
+pattern_1()
+
